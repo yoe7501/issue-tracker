4be5fc217f8f19669ed1afb8a56f498a6aa88e42
yoe7501 <yoel7501@gmail.com>
2024-10-10 21:37:20 -0400

    built styled link componenet

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import Spinner from "../components/Spinner";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
+import Link from "../components/Link";
 
 const  IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -24,7 +24,7 @@ const  IssuesPage = async () => {
           {issues.map(issue=> (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <Link href={`/issues/${issue.id}`} >{issue.title}</Link>
                 
                 <div className='block md:hidden'><IssueStatusBadge status={issue.status} /></div>
                 </Table.Cell>
