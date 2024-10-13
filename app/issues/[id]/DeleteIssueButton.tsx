de27d0bbb8b833fae7072a56e48f072221577afe
yoe7501 <yoel7501@gmail.com>
2024-10-12 21:20:12 -0400

    imporove user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,28 +1,34 @@
 "use client";
 
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex, Spinner } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
-  const [error, setError] = useState(false); // Single state to manage error condition and dialog visibility
+  const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
-    const deleteIssue = async () => {
-        try {
-          await axios.delete("/api/issues/" + issueId);
-          router.push("/issues");
-          router.refresh();
-        } catch (error) {
-          setError(true); // Set error to true when deletion fails
-        }
-      }
+  const deleteIssue = async () => {
+    try {
+      setDeleting(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setDeleting(false);
+      setError(true);
+    }
+  };
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button disabled={isDeleting} color="red">
+            Delete Issue
+            {isDeleting && <Spinner/>}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -36,10 +42,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action>
-              <Button
-                color="red"
-                onClick={deleteIssue}
-              >
+              <Button color="red" onClick={deleteIssue}>
                 Delete Issue
               </Button>
             </AlertDialog.Action>
