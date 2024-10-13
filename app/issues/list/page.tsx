b4adfaa11e61e6547fe3efe23a9c5bb14a02a7c7
yoe7501 <yoel7501@gmail.com>
2024-10-12 21:31:47 -0400

    remove duplicate skeleton

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import Link from "../components/Link";
+import IssueStatusBadge from "../../components/IssueStatusBadge";
+import Link from "../../components/Link";
 import IssueActions from "./IssueActions";
 
 const  IssuesPage = async () => {
