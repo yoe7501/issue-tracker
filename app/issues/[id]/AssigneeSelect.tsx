73bf326996b3b483fdc4904a969a71c6c82e5f2f
yoe7501 <yoel7501@gmail.com>
2024-10-14 22:24:37 -0400

    show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,32 +1,46 @@
 "use client";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import toast, {Toaster} from 'react-hot-toast';
 
-const AssigneeSelect = () => {
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000,
-        retry: 3
-    });
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
+  if (isLoading) return <Skeleton />;
 
-    if (isLoading) return <Skeleton/>;
-
-    if(error) return null;
-
+  if (error) return null;
 
   return (
-    <Select.Root>
+    <>
+    <Select.Root
+    defaultValue={issue.assignedToUserId || "unassigned"}
+      onValueChange={(userId) => {
+        const assignedToUserId = userId === "unassigned" ? null : userId; // Handle "unassigned"
+        axios.patch("/api/issues/" + issue.id, { assignedToUserId })
+        .catch(()=> {
+            toast.error('Chnages could not be saved.');
+        });
+      }}
+    >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
+          <Select.Item value="unassigned">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {" "}
@@ -36,6 +50,8 @@ const AssigneeSelect = () => {
         </Select.Group>
       </Select.Content>
     </Select.Root>
+    <Toaster/>
+    </>
   );
 };
 
