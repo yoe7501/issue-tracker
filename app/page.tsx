fa216b13c4347be431b08f5921834b4a289e54bd
yoe7501 <yoel7501@gmail.com>
2024-10-16 14:53:37 -0400

    built the pagination layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-      <div> Hello world </div>
-   
+      <Pagination itemCount={100} pageSize={10} currentPage={2}/>
 
   );
 }
